629a3e11973df0f3644c1634f488d8bb61371da5
Dave Ho <daveho8888@gmail.com>
2020-05-09 21:46:33 -0700

    remove starter code from App.js

--- popfund/src/client/App.js
+++ popfund/src/client/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './app.css';
 import ReactImage from './react.png';
+import BusinessList from './components/BusinessList'
 
+/* shows example of fetch
 export default class App extends Component {
   state = { username: null };
 
@@ -17,6 +19,27 @@ export default class App extends Component {
       <div>
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
         <img src={ReactImage} alt="react" />
+      </div>
+    );
+  }
+}
+*/
+
+export default class App extends Component {
+  state = { username: null };
+
+  componentDidMount() {
+    fetch('/api/getUsername')
+      .then(res => res.json())
+      .then(user => this.setState({ username: user.username }));
+  }
+
+  render() {
+    const { username } = this.state;
+    return (
+      <div>
+        <h1>Welcome to popfund.</h1>
+        <BusinessList />
       </div>
     );
   }
---
+++ popfund/src/client/components/BusinessList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BusinessList extends Component {
+    
+    render() {
+        return (
+            <div>
+                <p>some sample text for business list</p>
+            </div>
+        );
+    }
+}
+
+export default BusinessList;
