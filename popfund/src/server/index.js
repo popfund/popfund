af499bcf3a332a64dfdd55be8814db1ac3d832ad
Dave Ho <daveho8888@gmail.com>
2020-05-09 21:09:36 -0700

    start skeleton of getBusinesses api

--- popfund/src/server/index.js
+++ popfund/src/server/index.js
@@ -9,6 +9,7 @@ async function listDatabases(client){
     databasesList.databases.forEach(db => console.log(` ~ ${db.name}`));
 }
 
+// remove this when done testing
 async function connectDB(){
     const mongo_uri = 'mongodb+srv://genuser:popfund@popfund-cluster-jxrtb.mongodb.net/test?retryWrites=true&w=majority';
     const client = new MongoClient(mongo_uri);
@@ -27,6 +28,18 @@ connectDB().catch(console.error);
 const app = express();
 
 app.use(express.static('dist'));
+
+// List of APIS
+app.get('/', (req, res) => res.send('GET request to the homepage'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/getBusinesses', (req, res) => {
+    console.log('here');
+    const lat = req.query.lat;
+    const long = req.query.long;
+    console.log(lat);
+    console.log(long);
+    res.send(`${lat}, ${long}`);
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
