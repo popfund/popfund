48a3e2b20361e96f26ce351e60041a07d4964561
Dave Ho <daveho8888@gmail.com>
2020-05-13 14:53:51 -0700

    Add sign-in UI components

--- popfund/src/client/index.js
+++ popfund/src/client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import SignIn from './components/SignIn'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SignIn />, document.getElementById('root'));
