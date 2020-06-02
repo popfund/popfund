261d9d692d5f5936d7058162dce88ea0745dc7e9
Dave Ho <daveho8888@gmail.com>
2020-06-01 21:21:09 -0700

    update favicon and title

--- popfund/webpack.config.js
+++ popfund/webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      favicon: './public/favicon.ico'
+      favicon: './public/purpMark.ico'
     })
   ]
 };
